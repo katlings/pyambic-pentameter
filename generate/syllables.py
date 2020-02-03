4755c1142fdaa7ef25150c30c96f598f2242537e
Kathryn Lingel <kathryn@lingel.net>
2020-02-03 03:16:05 -0800

    Docstrings!

--- generate/syllables.py
+++ generate/syllables.py
@@ -10,14 +10,26 @@ d = cmudict.dict()
 
 
 def clean_word(s):
+    """
+    Strip out selected punctuation and convert word to lower case, to normalize
+    it in preparation for looking it up in the pronunciation dictionary
+    """
     # we are actually gonna leave internal 's in for contraction purposes and
     # _s and .s in for "pronouncing code" purposes, as well as numbers
     return re.sub(r"[^a-z0-9\-_\.']+", '', s.lower().strip("'"))
 
 
 def count_vowel_groups(word):
-    # this is a first order approximation of number of syllables.
-    # it won't be correct on  e.g. aria, Julia, praying, antiestablishment
+    """
+    Iterate through the word to make a guess at how many syllables it has,
+    based on the number of groupings of adjacent vowels.
+
+    Includes some special cases to try to intelligently pronounce silent e,
+    digits, single letters, words with no vowels that might be acronyms, etc.
+
+    It won't be correct on words with adjacent vowels pronounced as different
+    syllables e.g. aria, Julia, praying, antiestablishment
+    """
     if not word:
         return 0
 
@@ -31,6 +43,7 @@ def count_vowel_groups(word):
     # special case for no vowels - maybe it's an acronym. we can say each
     # letter individually and they're probably all one syllable (except w)
     if all(vowel not in word for vowel in vowels):
+        # add extra syllables for w, 7, 0
         return len(word) + 2 * word.count('w') + word.count('7') + word.count('0')
 
     syllables = 0
@@ -55,6 +68,11 @@ def count_vowel_groups(word):
 
 
 def count_syllables(word):
+    """
+    Make a best guess at the number of syllables in the word, using the
+    pronunciation dictionary if available or the vowel groups approximation if
+    not.
+    """
     if ' ' in word:
         return sum([count_syllables(w) for w in word.split()])
 
@@ -73,7 +91,7 @@ def count_syllables(word):
     if not word in d:
         return count_vowel_groups(word)
 
-    sounds = d[word][0]
+    sounds = d[word][0]  # just pick the first pronunciation if there are multiple
     syllables = 0
     for s in sounds:
         if '0' in s or '1' in s or '2' in s:
@@ -82,6 +100,10 @@ def count_syllables(word):
 
 
 def get_syllable_stress(word):
+    """
+    Get all possible syllable stress options, represented as a list of strings
+    of 0 (unstressed), 1 (primary stress), and 2 (secondary stress)
+    """
     ends_with_ing = word.endswith("in'")
     word = clean_word(word)
     stresses_options = set()
@@ -101,17 +123,29 @@ def get_syllable_stress(word):
         for p in pronunciations:
             stress = []
             for syllable in p:
-                if '1' in syllable or '2' in syllable:  # for now, ignore whatever 2 means
+                if '1' in syllable:
                     stress.append('1')
+                if '2' in syllable:  # secondary stress
+                    stress.append('2')
                 if '0' in syllable:
-                    stress.append('0')  # todo: watch for two unstressed in a row
-                                        # ^ wtf self, what does this mean
+                    stress.append('0')
             stresses_options.add(''.join(stress))
 
     return stresses_options
 
 
 def syllable_fingerprint(word):
+    """
+    Use the pronuncation dict to map the potential syllable stress patterns
+    of a word to a ternary string "fingerprint"
+
+    0 is a syllable that must be unstressed
+    1 is a syllable that must be stressed
+    x is a syllable that may be stressed or unstressed
+
+    e.g. python => 10
+    pronunciation => 0x010
+    """
     stresses = get_syllable_stress(word)
     if not stresses:
         raise ValueError(f'Found no options for word {word}')
@@ -119,9 +153,9 @@ def syllable_fingerprint(word):
         return stresses.pop()
     syllables = len(list(stresses)[0])
     if not all(len(s) == syllables for s in stresses):
-        logging.debug('well crud we have to deal with multiple syllables here')
+        logging.debug('Multiple syllables found')
         logging.debug('%s, %s', word, stresses)
-        return stresses.pop()  # lol pick one. TODO
+        return stresses.pop()  # lol just pick one. TODO
     fp = []
     for i in range(syllables):
         if all(s[i] == '1' for s in stresses):
@@ -133,15 +167,14 @@ def syllable_fingerprint(word):
     return ''.join(fp)
 
 
-def get_sequence_fingerprint(sequence):
-    words = sequence.split()
-    fps = []
-    for word in words:
-        fps.append(syllable_fingerprint(word))
-    return ''.join(fps)
-
-
 def rhyme_fingerprint(word):
+    """
+    Return a representation of the pronunciation of the last set of sounds in
+    the word, starting at the last emphasized syllable
+
+    In other words, return the sound that this word ends with, in order to find
+    words that rhyme
+    """
     word = word.lower()
     if not word in d:
         return None
@@ -152,6 +185,7 @@ def rhyme_fingerprint(word):
     sounds = d[word][0]
     fingerprint = []
 
+    # there's probably a way to do this with list.find() or something
     for sound in sounds[::-1]:
         fingerprint.append(sound)
         # digits designate emphasis in syllables; we've found a syllable,
@@ -171,33 +205,24 @@ def scansion_matches(a, b):
 
 
 def fulfills_scansion(word, desired_fp):
+    """
+    True if the word's meter and the desired meter are compatible
+    """
     syl_fp = syllable_fingerprint(word)
-    return len(syl_fp) == len(desired_fp) and scansion_matches(syl_fp, desired_fp)
+    return scansion_matches(syl_fp, desired_fp)
 
 
 def valid_option(word, desired_fp):
+    """
+    True if the word's meter is compatible at the end of the desired meter
+    """
     syl_fp = syllable_fingerprint(word)
     return len(syl_fp) <= len(desired_fp) and scansion_matches(syl_fp, desired_fp[-len(syl_fp):])
 
 
 def remaining_scheme(word, remaining_syl):
+    """
+    Cut off the word's length and return the remaining meter to look for
+    """
     syl_fp = syllable_fingerprint(word)
     return remaining_syl[:-len(syl_fp)]
-
-
-def potentially_iambic(word, ends_on_stress):
-    syl_fp = syllable_fingerprint(word)
-    if ends_on_stress:
-        iamb_fp = '01' * (len(syl_fp)//2)
-        if len(syl_fp) % 2 == 1:
-            iamb_fp = '1' + iamb_fp
-    else:
-        iamb_fp = '10' * (len(syl_fp)//2)
-        if len(syl_fp) % 2 == 1:
-            iamb_fp = '0' + iamb_fp
-
-    return scansion_matches(syl_fp, iamb_fp)
-
-
-def potential_iambic_seed(word):
-    return potentially_iambic(word, True)
